8fd4528de1228cb70a8ca6e161c1d5f0ea8cead0
hurui <hurui19900515@gmail.com>
2017-05-22 21:10:14 +0800

    主页面添加菜单

--- app/src/main/java/com/example/hurui/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/hurui/kotlintest/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.hurui.kotlintest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textview!!.text = "Hurui";
+        textview!!.text = "Kotlin Test"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        var id = item?.itemId
+        if (id == R.id.action_setting){
+            toast("Setting", Toast.LENGTH_LONG)
+        }else if (id == R.id.action_about){
+            toast("About", Toast.LENGTH_SHORT)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun toast(message:String, length:Int){
+        Toast.makeText(this, message, length).show()
     }
 }
