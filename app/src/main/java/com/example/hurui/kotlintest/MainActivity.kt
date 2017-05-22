3f43fee55977b76fc1ff42c0dbf879a5fa09c1fe
hurui <hurui19900515@gmail.com>
2017-05-22 22:14:19 +0800

    添加了Toolebar

--- app/src/main/java/com/example/hurui/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/hurui/kotlintest/MainActivity.kt
@@ -1,26 +1,34 @@
 package com.example.hurui.kotlintest
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.view.Menu
 import android.view.MenuItem
+import android.view.Window
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.*//kotlin-android-extensions插件的作用，可以直接引用该布局中的id
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = resources.getString(R.string.first_page)
 
-        textview!!.text = "Kotlin Test"
+        fab.setOnClickListener { view -> Snackbar.make(view, "Snackbar", Snackbar.LENGTH_SHORT).show() }
+
     }
 
+    //创建菜单
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
+    //每个菜单选项的操作
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         var id = item?.itemId
         if (id == R.id.action_setting){
