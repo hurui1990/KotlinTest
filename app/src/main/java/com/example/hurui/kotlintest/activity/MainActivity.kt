9e22c6073f5f4c02954ff7fa93768e1c2e4ecbea
hurui <hurui19900515@gmail.com>
2017-05-25 20:01:33 +0800

    MVP框架结构分层

---
+++ app/src/main/java/com/example/hurui/kotlintest/activity/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.hurui.kotlintest.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hurui.kotlintest.R
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
--- app/src/main/java/com/example/hurui/kotlintest/bean/User.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hurui.kotlintest.bean
-
-/**
- * Created by hurui on 2017/5/22.
- */
-class User (var name:String, var age:Int)
